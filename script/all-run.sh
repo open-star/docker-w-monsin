5504e494a2eee05387cf39977f84cb08199fb8bb
Masaaki Hoshino <mahoshi60@gmail.com>
2016-02-11 02:32:10 -0500

    modified:   .dockerignore 	modified:   script/all-run.sh

--- script/all-run.sh
+++ script/all-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker run -itd --name mongo-data openstar/mongo-data
 docker run -d --name mongo --volumes-from=mongo-data mongo
-docker run -itd --name w-monsin --link mongo:db openstar/w-monsin
+docker run -p 3000:3000 -itd --name w-monsin --link mongo:db openstar/w-monsin
